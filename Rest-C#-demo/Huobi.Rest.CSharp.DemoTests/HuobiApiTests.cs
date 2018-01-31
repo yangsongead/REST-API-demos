77d68d5a4138db948db8b8d29ce6512ead60b072
Huobi-API <huobiapi@huobi.com>
2018-01-31 15:28:21 +0800

    Rest-C#-demo
    Rest-C#-demo

---
+++ Rest-C#-demo/Huobi.Rest.CSharp.DemoTests/HuobiApiTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Huobi.Rest.CSharp.Demo.Tests
+{
+    [TestClass()]
+    public class HuobiApiTests
+    {
+        HuobiApi api = new HuobiApi("","");
+        [TestMethod()]
+        public void GetAllAccountTest()
+        {
+            var result = api.GetAllAccount();
+            Assert.IsNull(result);
+        }
+    }
+}
---
+++ Rest-C#-demo/README.md
@@ -0,0 +1,9 @@
+# Huobi.Rest.CSharp.Demo
+# 项目地址：https://github.com/CryptocurrencyToolKits/Huobi.Rest.CSharp.Demo
+
+### Useage:
+```csharp
+    HuobiApi api = new HuobiApi("accessKey","seceretKey");
+    var result = api.GetAllAccount();
+```
+
